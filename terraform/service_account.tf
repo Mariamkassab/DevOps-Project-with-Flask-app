24cbfa2209a16ff8c8ac09a35e92e80ba6b7071e
Mariam Kassab <mariam.solimankassab@gmail.com>
2025-10-05 22:44:39 +0300

    github service account

--- terraform/service_account.tf
+++ terraform/service_account.tf
@@ -11,3 +11,24 @@ resource "google_project_iam_binding" "gke-service-account-iam" {
     "serviceAccount:${google_service_account.gke-service-account.email}"
   ]
 }
+
+
+
+resource "google_service_account" "github_deployer" {
+  account_id   = "github-deployer"
+  display_name = "GitHub Actions GKE Deployer"
+}
+
+# Grant container.admin role
+resource "google_project_iam_member" "container_admin_binding" {
+  project = "my-fun-playground"
+  role    = "roles/container.admin"
+  member  = "serviceAccount:${google_service_account.github_deployer.email}"
+}
+
+# Grant storage.admin role
+resource "google_project_iam_member" "storage_admin_binding" {
+  project = "my-fun-playground"
+  role    = "roles/storage.admin"
+  member  = "serviceAccount:${google_service_account.github_deployer.email}"
+}
